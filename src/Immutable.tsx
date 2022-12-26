87c369ecb4b3851c08b6f6e7f37b1e222b989573
二货机器人 <smith3816@gmail.com>
2022-12-26 16:38:37 +0800

    feat: add immutable

---
+++ src/Immutable.tsx
@@ -0,0 +1,45 @@
+import { supportRef } from 'rc-util/lib/ref';
+import * as React from 'react';
+
+const RenderContext = React.createContext<number>(0);
+
+export function makeImmutable<T extends React.ComponentType<any>>(Component: T): T {
+  const refAble = supportRef(Component);
+
+  const ImmutableComponent = function (props: any, ref: any) {
+    const refProps = refAble ? { ref } : {};
+    const renderTimesRef = React.useRef(0);
+    renderTimesRef.current += 1;
+
+    return (
+      <RenderContext.Provider value={renderTimesRef.current}>
+        <Component {...props} {...refProps} />
+      </RenderContext.Provider>
+    );
+  };
+
+  if (process.env.NODE_ENV !== 'production') {
+    ImmutableComponent.displayName = `ImmutableRoot(${Component.displayName || Component.name})`;
+  }
+
+  return refAble ? React.forwardRef(ImmutableComponent) : (ImmutableComponent as any);
+}
+
+export function responseImmutable<T extends React.ComponentType<any>>(Component: T): T {
+  const refAble = supportRef(Component);
+
+  const ImmutableComponent = function (props: any, ref: any) {
+    const refProps = refAble ? { ref } : {};
+    const renderTimes = React.useContext(RenderContext);
+
+    return React.useMemo(() => <Component {...props} {...refProps} />, [renderTimes]);
+  };
+
+  if (process.env.NODE_ENV !== 'production') {
+    ImmutableComponent.displayName = `ImmutableResponse(${
+      Component.displayName || Component.name
+    })`;
+  }
+
+  return refAble ? React.forwardRef(ImmutableComponent) : (ImmutableComponent as any);
+}
