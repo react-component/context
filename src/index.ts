921a08f9f09bd5489ec31a4c39a84e060dbbe554
二货机器人 <smith3816@gmail.com>
2022-12-26 16:55:44 +0800

    docs: immutable wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import type { SelectorContext } from './context';
 import { createContext, useContextSelector } from './context';
+import { makeImmutable, responseImmutable } from './Immutable';
 
-export { createContext, useContextSelector };
+export { createContext, useContextSelector, makeImmutable, responseImmutable };
 export type { SelectorContext };
