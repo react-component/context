b47404bfbe11140256fa55ce0b5d00a23fe553a6
二货机器人 <smith3816@gmail.com>
2022-12-27 14:30:30 +0800

    docs: patch doc info

--- src/Immutable.tsx
+++ src/Immutable.tsx
@@ -3,6 +3,11 @@ import * as React from 'react';
 
 const RenderContext = React.createContext<number>(0);
 
+/**
+ * Wrapped Component will be marked as Immutable.
+ * When Component parent trigger render,
+ * it will notice children component (use with `responseImmutable`) node that parent has updated.
+ */
 export function makeImmutable<T extends React.ComponentType<any>>(Component: T): T {
   const refAble = supportRef(Component);
 
@@ -25,6 +30,10 @@ export function makeImmutable<T extends React.ComponentType<any>>(Component: T):
   return refAble ? React.forwardRef(ImmutableComponent) : (ImmutableComponent as any);
 }
 
+/**
+ * Wrapped Component with `React.memo`.
+ * But will rerender when parent with `makeImmutable` rerender.
+ */
 export function responseImmutable<T extends React.ComponentType<any>>(Component: T): T {
   const refAble = supportRef(Component);
 
